560828bad52b1397755114f8ad9868777c06a878
Jahnvikadia <80562621+Jahnvikadia@users.noreply.github.com>
2022-02-07 17:04:35 +0530

    file_5 commit

---
+++ app/src/main/java/com/mc2022/template/SymptomData.java
@@ -0,0 +1,17 @@
+package com.mc2022.template;
+
+public class SymptomData {
+    private int que;
+
+    public SymptomData(int q){
+        que = q;
+    }
+
+    public int getQue(){
+        return que;
+    }
+
+    public void setQue(int q){
+        que = q;
+    }
+}
